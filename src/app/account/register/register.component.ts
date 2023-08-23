bd3fc6293b2d76d284a6a5a4663169e1719dfa68
andrey1de <andrey1de@gmail.com>
2023-08-24 01:45:04 +0300

    Перед основным шмоном

--- src/app/account/register/register.component.ts
+++ src/app/account/register/register.component.ts
@@ -1,7 +1,6 @@
 ﻿import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { first } from 'rxjs/operators';
 import {GKeybLanGlobal as G} from '@app/_globals'
 
 import { UsersAccountService, AlertService, GUser } from '@app/_services';
@@ -11,11 +10,14 @@ import { IUserDetailsFieldsData, USER_DATA_MULTI } from './register.data';
 import { ILANG_DESCR } from '@app/keyboard/keyb-data/keyb.data';
 import { Subscription } from 'rxjs';
 import { UserModel } from '@app/_models';
+
+const TO_TEST_USER = true;
+
 @Component({ 
   selector: 'and-register',
  
   templateUrl: 'register.component.html',
-  styleUrls: ['register.component.scss']
+  styleUrls:  ['register.component.scss']
 
  })
 export class RegisterComponent implements OnInit ,OnDestroy{
@@ -33,23 +35,8 @@ export class RegisterComponent implements OnInit ,OnDestroy{
         private alertSvc: AlertService
     ) { }
   get user() {return GUser;}
-  toEnter(): boolean {
-   // this.userSvc.clearFlags();
-     // this.flags.fHome = true;//=1;
-    // this.flags.fLogout = false;//=2;
-    //  this.flags.fLogin = true;//=4;
-    //  this.flags.fRegistr = false;//=8;
-    //  this.flags.fVisa = false;//=16;
-    // this.flags.fPay = false;//=32;
-    //  this.flags.fAdmin = false;// = 128
-    return true;
-  }
-  get state(): epg {
-   return epg.eRegistrate;
-  }
-  toExit(): boolean {
-   return true;
-  }
+  
+
   model?: UserModel | undefined;
   get itsOK(): boolean {
     return this.form.valid;
@@ -58,21 +45,22 @@ export class RegisterComponent implements OnInit ,OnDestroy{
   
 
     public set Lang(v:TLangNames) {
-      if(this.first || v != this._Lang){
-        this.first = false;
+        
+     // if( v != this._Lang){
         this._Lang = v;
         this._onLangChange(v);
-      }
+     // }
+     
     }
   
-    private _Lang : TLangNames = 'en';
-    public get () : TLangNames {
+    private _Lang : TLangNames = G.Lang;
+    public get Lang() : TLangNames {
       return this._Lang;
     }
     private subs:Subscription[] = [];
     flds !: IUserDetailsFieldsData;
   
-    private first:boolean = true;
+   
     ngOnDestroy(): void {
       this.subs.forEach(s=>s.unsubscribe());
       G.KeyboardVisible = false;
@@ -81,12 +69,15 @@ export class RegisterComponent implements OnInit ,OnDestroy{
     ngOnInit() {
        G.KeyboardVisible = true;
         this.subs.push(G.Lang$.subscribe(lang=>this.Lang = lang));
-        this._createRegisterForm();
+        this.Lang = G.Lang;
+      this._createRegisterForm();
 
     }
     
 
+
     private _createRegisterForm() {
+
       this.form =  new FormGroup({
         firstName: new FormControl<string>('', [
           LangValidator.required("firstName")
@@ -103,7 +94,7 @@ export class RegisterComponent implements OnInit ,OnDestroy{
        
         passport: new FormControl('', [
               LangValidator.required("passport"),
-              LangValidator.number("passport",9,9)
+              LangValidator.teudatZehut("passport"),
             ]),
         email: new FormControl('', [
             LangValidator.required("email"),
@@ -128,10 +119,26 @@ export class RegisterComponent implements OnInit ,OnDestroy{
           LangValidator.requiredTrue("imagreePolicy")
         ]),
       });
-     
+
+      if(TO_TEST_USER){
+        this.f['firstName'].setValue('Avi') ;
+        this.f['lastName'].setValue('Cohen') ;
+        this.f['sysName'].setValue('avi1cohen') ;
+        this.f['password'].setValue('11111111') ;
+        this.f['passport'].setValue('999999998') ;
+        this.f['email'].setValue('avi1cohen@gmail.com') ;
+        this.f['phone'].setValue('05451111111') ;
+        this.f['address'].setValue('Hahistadrut 1/1 Petah Tikva') ;
+        this.f['ravkav'].setValue('111') ;
+        this.f['imagreeTerms'].setValue(true) ;
+        this.f['imagreePolicy'].setValue(true) ;
+
+      }
+           
            
     }
   
+    
   
     OnSliderChange(evt:any,ctrlName:string){ //'imagreePolicy'
 
@@ -149,26 +156,30 @@ export class RegisterComponent implements OnInit ,OnDestroy{
       //this change language for validation strings
      // LangValidator.Lang = v;
       /// To event !!!
+     // this._Lang = v;
       this.flds = USER_DATA_MULTI[this._Lang] as IUserDetailsFieldsData;//{...USER_DATA_MULTI[this._Lang]};
       console.log(`Set Lang ${v}:${ILANG_DESCR[v].name}`);
       this._validateMe();
   
     }
     private _validateMe() {
-      try {
-        this.form.markAllAsTouched();
+   //   try {
+        if(this.form){
+          this.form.markAllAsTouched();
 
-        for (let controlName in this.form?.controls) {
-          const c = this.f[controlName] as FormControl;
-          if(c){
-            c.markAsTouched();
-            c.updateValueAndValidity();
-          }
+          for (let controlName in this.form?.controls) {
+            const c = this.f[controlName] as FormControl;
+            if(c){
+              c.markAsTouched();
+              c.updateValueAndValidity();
+            }
+          
+          }        
+  
+        }
+  //    } catch (error) {
         
-        }        
-      } catch (error) {
-        
-      }
+  //    }
     }
 
     // convenience getter for easy access to form fields
